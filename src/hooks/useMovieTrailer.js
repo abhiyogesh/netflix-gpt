882bf31300aa24edf26d217b0a5a28742cc741b9
abhiyogesh <110718689+abhiyogesh@users.noreply.github.com>
2025-08-13 19:45:37 +0530

    namaste-hooks

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,29 +1,29 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
-import { addTrailerVideo } from "../utils/movieSlice";
+import { addTrailerVideo } from "../utils/moviesSlice";
 
-const useMovieTrailer =(movieId)=>{
-    const dispatch = useDispatch();
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
 
-    //fetch trailer video && updating the store with trailer video data
-    const getMoviesVideos = async ()=>{
-        const data = await fetch(
-            "https://api.themoviedb.org/3/movie/" +
-            movieId +
-            "/videos?language=en-US", 
-            API_OPTIONS);
-        const json = await data.json();
-        const filterData = json.results.filter((video)=>video.type === "Trailer");
-        const trailer  = filterData.length ? filterData[0] : json.results[0];
-        dispatch(addTrailerVideo(trailer))
-    
-    };
+  const trailerVideo = useSelector((store) => store.movies.trailerVideo);
 
-    useEffect(()=>{
-    getMoviesVideos();
-    },[]);
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
 
-}
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    dispatch(addTrailerVideo(trailer));
+  };
+  useEffect(() => {
+    !trailerVideo && getMovieVideos();
+  }, []);
+};
 
 export default useMovieTrailer;
--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -1,15 +1,15 @@
-import { useDispatch } from "react-redux";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
-import { addPopularMovies} from "../utils/movieSlice";
-
-
+import { addPopularMovies } from "../utils/moviesSlice";
 
 const usePopularMovies = () => {
-  //Fetch Data from TMDB API and update store  
+  // Fetch Data from TMDB API and update store
   const dispatch = useDispatch();
-  
-  const getPopularMoviess = async () => {
+
+  const popularMovies = useSelector((store) => store.movies.popularMovies);
+
+  const getPopularMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/popular?page=1",
       API_OPTIONS
@@ -19,7 +19,7 @@ const usePopularMovies = () => {
   };
 
   useEffect(() => {
-    getPopularMoviess();
+    !popularMovies && getPopularMovies();
   }, []);
 };
 
