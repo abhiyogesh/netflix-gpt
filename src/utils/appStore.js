10ed8864cfaa4ba5f3f4f4b7d7c619be91508782
abhiyogesh <110718689+abhiyogesh@users.noreply.github.com>
2025-08-13 19:48:14 +0530

    namaste-utils

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,13 +1,16 @@
-import {configureStore} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
-import moviesReducer from "./movieSlice";
-
+import moviesReducer from "./moviesSlice";
+import gptReducer from "./gptSlice";
+import configReducer from "./configSlice";
 
 const appStore = configureStore({
-    reducer: {
-    user : userReducer,
-    movies : moviesReducer,
-    },
+  reducer: {
+    user: userReducer,
+    movies: moviesReducer,
+    gpt: gptReducer,
+    config: configReducer,
+  },
 });
 
 export default appStore;
